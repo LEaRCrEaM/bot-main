e5310c07e4feb1e5a20d36fcc679cd9bcde30ccd
Fawad <ahmadfawadkabiri@gmail.com>
2025-06-03 20:55:59 -0400

    j

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ var premiumRanks = {
   31: "https://tankionline.com/play/static/images/31.06d52fbe.webp"
 }
 
-var hidden = ['Splxff', /*'ISwissCheeseYoAhh'*/'OTF', '0.00', 'Erina'];
+var hidden = ['Splxff', /*'ISwissCheeseYoAhh'*/'OTF', '0.00', 'Erina', 'aklsjdipwnpqowievksq'];
 
 client.on('interactionCreate', async interaction => {
   if (!interaction.isChatInputCommand()) return;
